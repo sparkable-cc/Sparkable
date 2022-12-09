14968f38a8f315fd16c1de4dfba048e547c50da6
Hynnot <hynnot@gmail.com>
2022-12-09 13:46:15 +0100

    Fixing unit tests

--- backend/src/contexts/users/actions/SignInAction.test.ts
+++ backend/src/contexts/users/actions/SignInAction.test.ts
@@ -1,21 +1,11 @@
 import { describe, expect, test } from '@jest/globals';
 import { UserNotFoundException } from '../domain/exceptions/UserNotFoundException';
 import { WrongPasswordException } from '../domain/exceptions/WrongPasswordException';
+import { User } from '../domain/models/User';
 import { UserRepositoryInMemory } from '../infrastructure/persistence/repositories/UserRepositoryInMemory';
 import { SignInAction } from './SignInAction';
 
 describe('signing in', () => {
-  test('can sign in', async () => {
-    const userRepository = new UserRepositoryInMemory();
-    // const createUserAction = new CreateUserAction(userRepository);
-    const signInAction = new SignInAction(userRepository);
-    const username = 'username';
-    const password = 'password';
-    await SignInAction.execute(username, 'password');
-
-    const user = await signInAction.execute(username, password);
-    expect(user.username).toEqual(username);
-  });
 
   test('cant sign in because the user does not exist', async () => {
     const userRepository = new UserRepositoryInMemory();
@@ -28,14 +18,26 @@ describe('signing in', () => {
 
   test('cant sign in because the password is wrong', async () => {
     const userRepository = new UserRepositoryInMemory();
-    // const createUserAction = new CreateUserAction(userRepository);
-    const signInAction = new SignInAction(userRepository);
     const username = 'username';
     const password = 'password';
-    await SignInAction.execute(username, password);
+    userRepository.storeUser(new User('email', username, password));
+    const signInAction = new SignInAction(userRepository);
 
     await expect(
       signInAction.execute(username, 'wrong password'),
     ).rejects.toThrow(WrongPasswordException);
   });
+
+  test('can sign in', async () => {
+    const userRepository = new UserRepositoryInMemory();
+    const username = 'username';
+    const password = 'password';
+    userRepository.storeUser(new User('email', username, password));
+    const signInAction = new SignInAction(userRepository);
+
+    const user = await signInAction.execute(username, password);
+
+    expect(user.username).toEqual(username);
+  });
+
 });
