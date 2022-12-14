8ab00aa587ebe6db25ffb35232830455392f1d56
Aumarh <aumarh@riseup.net>
2022-12-14 14:18:26 +0100

    Add auth0 integration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,46 +1,76 @@
 import express, { Express, Request, Response } from 'express';
-import { AppDataSource, TestDataSource } from './data-source';
-
+import { auth } from 'express-oauth2-jwt-bearer';
+import jwksClient from 'jwks-rsa';
 import { CreateUserAction } from './contexts/users/actions/CreateUserAction';
 import { EmailExistsException } from './contexts/users/domain/exceptions/EmailExistsException';
 import { MandatoryFieldEmptyException } from './contexts/users/domain/exceptions/MandatoryFieldEmptyException';
 import { UsernameExistsException } from './contexts/users/domain/exceptions/UsernameExistsException';
 import { UserRepositoryPG } from './contexts/users/infrastructure/persistence/repositories/UserRepositoryPG';
+import { AppDataSource, TestDataSource } from './data-source';
 
 let dataSource = AppDataSource;
 if (process.env.NODE_ENV === 'test') dataSource = TestDataSource;
 
 const app: Express = express();
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Butterfy API');
 });
 
-app.post('/user', async (req: Request, res: Response) => {
-  const createUserAction = new CreateUserAction(new UserRepositoryPG(dataSource));
-  createUserAction.execute(req.body.email, req.body.username, req.body.password)
-  .then(() => {
-    res.status(201);
-    res.send('User created!');
-  })
-  .catch((error) => {
-    switch (error.constructor) {
-      case MandatoryFieldEmptyException:
-        res.status(400);
-        res.send('Bad request');
-        break;
-      case UsernameExistsException || EmailExistsException:
-        res.status(403);
-        res.send('User exist!');
-        break;
-      default:
-        console.log('Failed to do something async with an unspecified error: ', error);
-        return res.send(500);
-    }
+const checkJwt = auth({
+  issuerBaseURL: 'https://localhost:5000',
+  audience: 'butterfy',
+});
+
+// This route doesn't need authentication
+app.get('/public', function (req, res) {
+  res.json({
+    message:
+      "Hello from a public endpoint! You don't need to be authenticated to see this.",
   });
+});
+
+// This route needs authentication
+app.get('/private', checkJwt, function (req, res) {
+  res.json({
+    message:
+      'Hello from a private endpoint! You need to be authenticated to see this.',
+  });
+});
+
+app.listen(3000, () => console.log('listening at http://localhost:3000'));
+
+app.post('/user', async (req: Request, res: Response) => {
+  const createUserAction = new CreateUserAction(
+    new UserRepositoryPG(dataSource),
+  );
+  createUserAction
+    .execute(req.body.email, req.body.username, req.body.password)
+    .then(() => {
+      res.status(201);
+      res.send('User created!');
+    })
+    .catch((error) => {
+      switch (error.constructor) {
+        case MandatoryFieldEmptyException:
+          res.status(400);
+          res.send('Bad request');
+          break;
+        case UsernameExistsException || EmailExistsException:
+          res.status(403);
+          res.send('User exist!');
+          break;
+        default:
+          console.log(
+            'Failed to do something async with an unspecified error: ',
+            error,
+          );
+          return res.send(500);
+      }
+    });
 });
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,19 @@
-import app from "./app";
 import dotenv from 'dotenv';
-import { AppDataSource } from "./data-source"
+import jwksClient from 'jwks-rsa';
+import app from './app';
+import { AppDataSource } from './data-source';
 
 dotenv.config();
 
 const port = process.env.PORT;
 
-AppDataSource
-  .initialize()
+AppDataSource.initialize()
   .then(() => {
-      console.log("Data Source has been initialized!")
+    console.log('Data Source has been initialized!');
   })
   .catch((err) => {
-      console.error("Error during Data Source initialization:", err)
-  })
+    console.error('Error during Data Source initialization:', err);
+  });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
