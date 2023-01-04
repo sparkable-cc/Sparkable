caced31fa7d6a88c4a6003af17d363b686413c39
Aumarh <aumarh@riseup.net>
2023-01-04 15:51:54 +0100

    Add first version of sort ui design

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,18 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { IoMdMenu } from 'react-icons/io';
+import { RiExternalLinkLine } from 'react-icons/ri';
 import styles from '../styles/Home.module.css';
-import { RiExternalLinkLine } from "react-icons/ri";
-
-import { AiOutlineMenu } from "react-icons/ai";
-import { IoMdMenu } from "react-icons/io";
-
-
 
 export async function getStaticProps() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL_API;
   const res = await fetch(`${baseUrl}/links/`);
   const links = await res.json();
   return {
-      props: {
-          links
-      }
+    props: {
+      links,
+    },
   };
 }
 
@@ -43,57 +40,80 @@ export default function Home({ links }: any) {
 
       <main className={styles.main}>
         <div>
-          Sparkable is a collection of insightful content, sourced and curated by the people.<br></br>
+          Sparkable is a collection of insightful content, sourced and curated
+          by the people.<br></br>
         </div>
-
-        <div className="break"><hr/></div>
+        <div>
+          <div>
+            <select className={styles.sort}>
+              <option className="dropdown-content" value="random">
+                Random
+              </option>
+              <option className="dropdown-content" value="newest first">
+                Newest First
+              </option>
+            </select>
+          </div>
+        </div>
+        <div className="break">
+          <hr />
+        </div>
 
         <div id="results">{links.total} results</div>
 
         <div className={styles.grid}>
-        {
-          links.links.map( ({title, link, username, image}:any, i:number) => {
-            const url = (new URL(link));
-            const domain = url.hostname.replace('www.','');
+          {links.links.map(
+            ({ title, link, username, image }: any, i: number) => {
+              const url = new URL(link);
+              const domain = url.hostname.replace('www.', '');
 
-            //PAR OR IMPAR
-            if ((i % 2) == 0) {
-              return (
-                <div className={styles.card}>
-                  <div className={styles.cardText}>
-                    <h2>{title}</h2>
-                    <p>Submitted by @{username}</p>
-                    <p>
-                      <a target="_blank" href={link} rel="noopener noreferrer">
-                        {domain}<RiExternalLinkLine />
-                      </a>
-                    </p>
+              //PAR OR IMPAR
+              if (i % 2 == 0) {
+                return (
+                  <div className={styles.card}>
+                    <div className={styles.cardText}>
+                      <h2>{title}</h2>
+                      <p>Submitted by @{username}</p>
+                      <p>
+                        <a
+                          target="_blank"
+                          href={link}
+                          rel="noopener noreferrer"
+                        >
+                          {domain}
+                          <RiExternalLinkLine />
+                        </a>
+                      </p>
+                    </div>
+                    <div className={styles.cardImage}>
+                      <img src="https://i.picsum.photos/id/599/200/200.jpg?hmac=2WLKs3sxIsaEQ-6WZaa6YMxgl6ZC4cNnid0aqupm2is"></img>
+                    </div>
                   </div>
-                  <div className={styles.cardImage}>
-                    <img src='https://i.picsum.photos/id/599/200/200.jpg?hmac=2WLKs3sxIsaEQ-6WZaa6YMxgl6ZC4cNnid0aqupm2is'></img>
+                );
+              } else {
+                return (
+                  <div className={styles.card}>
+                    <div className={styles.cardText}>
+                      <h2>{title}</h2>
+                      <p>Submitted by @{username}</p>
+                      <p>
+                        <a
+                          target="_blank"
+                          href={link}
+                          rel="noopener noreferrer"
+                        >
+                          {domain}
+                          <RiExternalLinkLine />
+                        </a>
+                      </p>
+                    </div>
                   </div>
-                </div>
-              )
-            } else {
-              return (
-                <div className={styles.card}>
-                  <div className={styles.cardText}>
-                    <h2>{title}</h2>
-                    <p>Submitted by @{username}</p>
-                    <p>
-                      <a target="_blank" href={link} rel="noopener noreferrer">
-                        {domain}<RiExternalLinkLine />
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              )
-            }
-          })
-        }
+                );
+              }
+            },
+          )}
         </div>
       </main>
-
     </div>
   );
 }
