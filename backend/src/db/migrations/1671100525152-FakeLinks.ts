62f65f9f1c444163619145a250a3f559bbbe1f2a
Hynnot <hynnot@gmail.com>
2023-01-09 18:48:09 +0100

    Fix error in migration

--- backend/src/db/migrations/1671100525152-FakeLinks.ts
+++ backend/src/db/migrations/1671100525152-FakeLinks.ts
@@ -1,17 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
-import { LinkEntity } from "../../contexts/links/infrastructure/persistence/entities/LinkEntity";
 import LinkFactory from "../../factories/LinkFactory";
 
 export class FakeLinks1671100525152 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         const linksDto = LinkFactory.createX(10);
-
-        linksDto.forEach( async linkDto => {
-            await queryRunner.manager.save(
-                queryRunner.manager.create<LinkEntity>(LinkEntity,linkDto)
-            );
-        });
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
