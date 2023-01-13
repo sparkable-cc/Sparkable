fc276b01d0b88063ff1c54be1d89cd0a8f0428fb
Dima <dimakrsna@gmail.com>
2023-01-13 18:54:21 +0200

    Welcome screen

---
+++ frontend/components/Welcome/index.tsx
@@ -0,0 +1,34 @@
+import styles from './index.module.scss';
+import classNames from 'classnames';
+import { ReactSVG } from 'react-svg';
+
+export const Welcome = () => {
+
+  return (
+    <div className={styles.welcomeWrapper}>
+      <h1 className={styles.title}>Spark new insight.</h1>
+      <p className={styles.subtitle}>Find insightful content. Crowd-curated and powered by a new reward system.</p>
+      <div className={styles.buttonsWrapper}>
+        <button className={classNames(styles.startButton, styles.sizeXl)}>Start Exploring</button>
+        <button className={classNames(styles.buttonTransparent, styles.sizeXl)}>Learn More</button>
+      </div>
+      <div className={styles.benefitsWrapper}>
+        <div className={styles.benefit}>
+          <ReactSVG className={styles.benefitIcon} src="/svg/lens.svg" />
+          <h2 className={styles.benefitTitle}>Discover and submit</h2>
+          <p className={styles.benefitDescription}>View and send in links that had a lasting impact.</p>
+        </div>
+        <div className={styles.benefit}>
+          <ReactSVG className={styles.benefitIcon} src="/svg/page.svg" />
+          <h2 className={styles.benefitTitle}>Vote monthly</h2>
+          <p className={styles.benefitDescription}>Select the most insightful and constructive content.</p>
+        </div>
+        <div className={styles.benefit}>
+          <ReactSVG className={styles.benefitIcon} src="/svg/stars.svg" />
+          <h2 className={styles.benefitTitle}>Reward and get rewarded</h2>
+          <p className={styles.benefitDescription}>Earn rewards for providing new insight.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,20 +1,21 @@
 import type { NextPage } from 'next';
 import { Sidebar } from '../components/Sidebar';
 import { AuthButtons } from '../components/AuthButtons';
+import { Welcome } from '../components/Welcome';
 import styles from '../styles/Home.module.scss';
 
 const HomePage: NextPage = () => {
   return (
     <main className={styles.mainWrapper}>
-      <Sidebar/>
+      <Sidebar />
       <section className={styles.container}>
         <header className={styles.header}>
-          <AuthButtons/>
+          <AuthButtons />
         </header>
+        <Welcome />
       </section>
     </main>
   )
 }
-
 
 export default HomePage;
