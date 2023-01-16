b31e9b7bcf37c24b87db8cdcb1467de0ae62993a
Hynnot <hynnot@gmail.com>
2023-01-16 18:21:15 +0100

    extract database variable to env file

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,13 +2,15 @@ import { DataSource } from "typeorm"
 import "reflect-metadata";
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: "db",
-    port: 5432,
-    username: "user",
-    password: "password",
-    database: "db",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: true,
     entities: [__dirname + '/contexts/**/infrastructure/persistence/entities/*.{ts,js}'],
