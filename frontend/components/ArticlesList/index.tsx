063800d4adc90144e382244d51932b525ff35747
Dima <dimakrsna@gmail.com>
2023-01-17 19:44:27 +0200

    ArticlesList component

---
+++ frontend/components/ArticlesList/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import { ArticleItem } from '../ArticleItem';
+import { useLazyGetLinksQuery } from '../../store/api/articles';
+import { v4 as uuidv4 } from 'uuid';
+import { Spiner } from '../Spiner';
+import classNames from 'classnames';
+import styles from './index.module.scss';
+
+export const ArticlesList = () => {
+  const [triggerGetLinks, { isLoading, data }] = useLazyGetLinksQuery();
+
+  const onGetArticles = () => {
+    triggerGetLinks();
+  };
+
+  useEffect(() => {
+    if (!data) {
+      onGetArticles();
+    }
+  }, [data]);
+
+  return (
+    <section className={styles.articlesWrapper} id="explore">
+      <div className={styles.exploreTitleWrapper}>
+        <div className={styles.exploreButton} />
+        <h2 className={styles.exploreTitle}>
+          <span>Explore</span> what others have submitted</h2>
+      </div>
+      <section className={styles.articlesList}>
+        {data?.links?.length &&
+          data.links.map(item => <ArticleItem
+            {...item}
+            key={uuidv4()}
+          />)}
+      </section>
+      {isLoading && <Spiner wrapperClassName={styles.spinnerWrapper} />}
+      <div className={styles.loadMoreWrapper}>
+        <button className={classNames(styles.loadMoreButton, styles.disable)}>Load more</button>
+      </div>
+    </section>
+  )
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,28 +1,12 @@
-import { useEffect } from 'react';
 import type { NextPage } from 'next';
 import { Sidebar } from '../components/Sidebar';
 import { AuthButtons } from '../components/AuthButtons';
 import { Welcome } from '../components/Welcome';
-import { ArticleItem } from '../components/ArticleItem';
 import { Filters } from '../components/Filters';
 import styles from '../styles/Home.module.scss';
-import { useLazyGetLinksQuery } from '../store/api/articles';
-import { v4 as uuidv4 } from 'uuid';
-import { Spiner } from '../components/Spiner';
-import classNames from 'classnames';
+import { ArticlesList } from '../components/ArticlesList';
 
 const HomePage: NextPage = () => {
-  const [triggerGetLinks, { isLoading, data }] = useLazyGetLinksQuery();
-
-  const onGetArticles = () => {
-    triggerGetLinks();
-  };
-
-  useEffect(() => {
-    if (!data) {
-      onGetArticles();
-    }
-  }, [data]);
 
   return (
     <main className={styles.mainWrapper}>
@@ -31,24 +15,7 @@ const HomePage: NextPage = () => {
         <AuthButtons />
         <Welcome />
         <div className={styles.contentWrapper}>
-          <section className={styles.articlesWrapper} id="explore">
-            <div className={styles.exploreTitleWrapper}>
-              <div className={styles.exploreButton} />
-              <h2 className={styles.exploreTitle}>
-                <span>Explore</span> what others have submitted</h2>
-            </div>
-            <section className={styles.articlesList}>
-              {data?.links?.length &&
-                data.links.map(item => <ArticleItem
-                  {...item}
-                  key={uuidv4()}
-                />)}
-            </section>
-            {isLoading && <Spiner wrapperClassName={styles.spinnerWrapper}/>}
-            <div className={styles.loadMoreWrapper}>
-              <button className={classNames(styles.loadMoreButton, styles.disable)}>Load more</button>
-            </div>
-          </section>
+          <ArticlesList />
           <Filters />
         </div>
       </section>
