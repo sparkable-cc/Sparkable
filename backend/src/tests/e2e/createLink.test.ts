9cb86b6273aba2e50539329a90fbdac1db6d0ef9
Aumarh <aumarh@riseup.net>
2023-01-25 16:42:45 +0100

    Add create link e2e test

---
+++ backend/src/tests/e2e/createLink.test.ts
@@ -0,0 +1,62 @@
+import request from 'supertest';
+import app from '../../app';
+import { LinkEntity } from '../../contexts/links/infrastructure/persistence/entities/LinkEntity';
+import dataSource from '../../data-source';
+
+describe('POST /link', () => {
+  beforeAll(async () => {
+    await dataSource.initialize();
+  });
+
+  afterAll(async () => {
+    await dataSource.destroy();
+  });
+
+  afterEach(async () => {
+    const repository = dataSource.getRepository(LinkEntity);
+    await repository.clear();
+  });
+
+  it('returns 400 when the mandatory field is empty', async () => {
+    const req = await request(app)
+      .post('/link')
+      .send({
+        title: 'title',
+        url: 'http://example',
+        categories: ['Environment'],
+      });
+
+    expect(req.statusCode).toEqual(400);
+  });
+
+  it('returns 201 when the link is created', async () => {
+    const req = await request(app)
+      .post('/link')
+      .send({
+        title: 'title',
+        url: 'http://example',
+        categories: ['Environment'],
+      });
+
+    expect(req.statusCode).toEqual(201);
+  });
+
+  it('returns 403 when one unique field exists with the same value', async () => {
+    await request(app)
+      .post('/link')
+      .send({
+        title: 'title',
+        url: 'http://example',
+        categories: ['Environment'],
+      });
+
+    const req = await request(app)
+      .post('/link')
+      .send({
+        title: 'title',
+        url: 'http://example',
+        categories: ['Environment'],
+      });
+    expect(req.statusCode).toEqual(403);
+  });
+});
