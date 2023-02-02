b13d565760f85368fa20b732ddee426cefe71351
Dima <dimakrsna@gmail.com>
2023-02-02 13:17:31 +0200

    minor fix

--- frontend/components/ArticleItem/index.tsx
+++ frontend/components/ArticleItem/index.tsx
@@ -2,6 +2,7 @@ import styles from "./index.module.scss";
 import { ApiTypes } from "../../types";
 import { ArticleLink } from '../ArticleLink';
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 type Props = ApiTypes.Model.Link
 
@@ -14,16 +15,17 @@ export const ArticleItem = ({
   date,
   username
 }: Props) => {
+  const router = useRouter();
+
   return (
     <article className={styles.articleItem}>
-      <Link href={`/article/${id}`}>
-        {image && <div
-          className={styles.articleCover}
-          style={{
-            backgroundImage: `url(${image})`
-          }}
-        />}
-      </Link>
+      {image && <div
+        className={styles.articleCover}
+        onClick={() => { router.push(`/article/${id}`) }}
+        style={{
+          backgroundImage: `url(${image})`
+        }}
+      />}
       <div className={styles.articleInfoWrapper}>
         <ArticleLink link={link} />
         <Link className={styles.articleTitle} href={`/article/${id}`}>
