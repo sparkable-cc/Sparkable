95d9519017f736fcef139cf094e4dd3e9d446a54
Dima <dimakrsna@gmail.com>
2023-02-02 11:58:55 +0200

    BackButton component

---
+++ frontend/components/BackButton/index.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from 'react';
+import styles from './index.module.scss';
+import { useRouter } from 'next/router';
+
+interface Props extends PropsWithChildren {
+  directPath?: string
+}
+
+export const BackButton = ({ children, directPath }: Props) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    if(directPath) {
+      router.push(directPath)
+    } else {
+      router.back()
+    }
+  }
+
+  return (
+    <button className={styles.backButton} onClick={onClick}>
+      {children}
+    </button>
+  )
+}
