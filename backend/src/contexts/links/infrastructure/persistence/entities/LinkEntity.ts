3224ef01d7a4ee9ff1f36fd1eddbad6a9e017c1a
tutorial <aumarh@tutorial.net>
2023-02-14 11:00:42 +0100

    Add description to link entity

--- backend/src/contexts/links/infrastructure/persistence/entities/LinkEntity.ts
+++ backend/src/contexts/links/infrastructure/persistence/entities/LinkEntity.ts
@@ -1,32 +1,45 @@
-import { Entity, Column, PrimaryGeneratedColumn, Generated, CreateDateColumn, ManyToMany, JoinTable} from "typeorm"
-import { LinkDto } from "../../../domain/models/LinkDto";
-import { CategoryEntity } from "./CategoryEntity"
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { LinkDto } from '../../../domain/models/LinkDto';
+import { CategoryEntity } from './CategoryEntity';
 
 @Entity('links')
 export class LinkEntity implements LinkDto {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    @Generated("uuid")
-    uuid: string;
+  @Column()
+  @Generated('uuid')
+  uuid: string;
 
-    @Column()
-    title:string;
+  @Column()
+  title: string;
 
-    @Column()
-    link:string;
+  @Column()
+  link: string;
 
-    @Column()
-    image:string;
+  @Column()
+  image: string;
 
-    @CreateDateColumn({ type: 'timestamptz' })
-    date: Date;
+  @Column()
+  description: string;
 
-    @Column()
-    username:string;
+  @CreateDateColumn({ type: 'timestamptz' })
+  date: Date;
 
-    @ManyToMany((type) => CategoryEntity, (category) => category.links, { eager: true })
-    @JoinTable()
-    categories: CategoryEntity[]
+  @Column()
+  username: string;
+
+  @ManyToMany((type) => CategoryEntity, (category) => category.links, {
+    eager: true,
+  })
+  @JoinTable()
+  categories: CategoryEntity[];
 }
