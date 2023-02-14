899f64fea6cb776ca002003e01276f555f55e6a7
tutorial <aumarh@tutorial.net>
2023-02-14 11:56:11 +0100

    Add description in link entity test

--- backend/src/tests/e2e/GetLinkById.test.ts
+++ backend/src/tests/e2e/GetLinkById.test.ts
@@ -1,43 +1,42 @@
-import app from "../../app";
-import request from "supertest";
-import dataSource from "../../data-source"
-import { LinkEntity } from "../../contexts/links/infrastructure/persistence/entities/LinkEntity";
-import LinkFactory from "../../factories/LinkFactory"
-import CategoryFactory from "../../factories/CategoryFactory"
-import { CategoryEntity } from "../../contexts/links/infrastructure/persistence/entities/CategoryEntity";
+import request from 'supertest';
+import app from '../../app';
+import { CategoryEntity } from '../../contexts/links/infrastructure/persistence/entities/CategoryEntity';
+import { LinkEntity } from '../../contexts/links/infrastructure/persistence/entities/LinkEntity';
+import dataSource from '../../data-source';
+import CategoryFactory from '../../factories/CategoryFactory';
+import LinkFactory from '../../factories/LinkFactory';
 
-describe("GET /links/:id", () => {
+describe('GET /links/:id', () => {
+  beforeAll(async () => {
+    await dataSource.initialize();
+  });
 
-    beforeAll(async () => {
-        await dataSource.initialize();
-    });
+  afterAll(async () => {
+    await dataSource.destroy();
+  });
 
-    afterAll(async () => {
-        await dataSource.destroy();
-    });
+  afterEach(async () => {
+    const linkRepository = dataSource.getRepository(LinkEntity);
+    await linkRepository.delete({});
 
-    afterEach(async () => {
-        const linkRepository = dataSource.getRepository(LinkEntity);
-        await linkRepository.delete({});
+    const categoryRepository = dataSource.getRepository(CategoryEntity);
+    await categoryRepository.delete({});
+  });
 
-        const categoryRepository = dataSource.getRepository(CategoryEntity);
-        await categoryRepository.delete({});
-    });
+  it('returns 400 when does not exists link', async () => {
+    const res = await request(app).get('/links/1');
 
-    it("returns 400 when does not exists link", async () => {
-        const res = await request(app).get("/links/1");
+    expect(res.statusCode).toEqual(400);
+  });
 
-        expect(res.statusCode).toEqual(400);
-    });
+  it('returns 200 with link when exists', async () => {
+    const link = await LinkFactory.create();
 
-    it("returns 200 with link when exists", async () => {
-        const link = await LinkFactory.create();
+    const res = await request(app).get('/links/' + link.id);
 
-        const res = await request(app).get("/links/" + link.id);
-
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.id).toEqual(link.id);
-        expect(res.body.title).toEqual(link.title);
-    });
-
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.id).toEqual(link.id);
+    expect(res.body.title).toEqual(link.title);
+    expect(res.body.description).toEqual(link.description);
+  });
 });
--- backend/src/tests/e2e/GetLinks.test.ts
+++ backend/src/tests/e2e/GetLinks.test.ts
@@ -1,164 +1,169 @@
-import app from "../../app";
-import request from "supertest";
-import dataSource from "../../data-source"
-import { LinkEntity } from "../../contexts/links/infrastructure/persistence/entities/LinkEntity";
-import LinkFactory from "../../factories/LinkFactory"
-import CategoryFactory from "../../factories/CategoryFactory"
-import { CategoryEntity } from "../../contexts/links/infrastructure/persistence/entities/CategoryEntity";
+import request from 'supertest';
+import app from '../../app';
+import { CategoryEntity } from '../../contexts/links/infrastructure/persistence/entities/CategoryEntity';
+import { LinkEntity } from '../../contexts/links/infrastructure/persistence/entities/LinkEntity';
+import dataSource from '../../data-source';
+import CategoryFactory from '../../factories/CategoryFactory';
+import LinkFactory from '../../factories/LinkFactory';
 
-describe("GET /links", () => {
+describe('GET /links', () => {
+  beforeAll(async () => {
+    await dataSource.initialize();
+  });
 
-    beforeAll(async () => {
-        await dataSource.initialize();
-    });
+  afterAll(async () => {
+    await dataSource.destroy();
+  });
 
-    afterAll(async () => {
-        await dataSource.destroy();
-    });
+  afterEach(async () => {
+    const linkRepository = dataSource.getRepository(LinkEntity);
+    await linkRepository.delete({});
 
-    afterEach(async () => {
-        const linkRepository = dataSource.getRepository(LinkEntity);
-        await linkRepository.delete({});
+    const categoryRepository = dataSource.getRepository(CategoryEntity);
+    await categoryRepository.delete({});
+  });
 
-        const categoryRepository = dataSource.getRepository(CategoryEntity);
-        await categoryRepository.delete({});
-    });
+  it('returns 200 and empty response when does not exist links', async () => {
+    const res = await request(app).get('/links');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ links: [], total: 0 });
+  });
 
-    it("returns 200 and empty response when does not exist links", async () => {
-        const res = await request(app).get("/links");
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toEqual({links:[], total:0});
-    });
+  it('returns 200 with link when exists one', async () => {
+    const linkDto = await LinkFactory.create();
 
-    it("returns 200 with link when exists one", async () => {
-        const linkDto = await LinkFactory.create();
+    const res = await request(app).get('/links');
 
-        const res = await request(app).get("/links");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(1);
+    expect(res.body.links[0].link).toEqual(linkDto.link);
+    expect(res.body.links[0].description).toEqual(linkDto.description);
+    expect(res.body.links[0].hasOwnProperty('date')).toBe(true);
+    expect(res.body.links[0].hasOwnProperty('uuid')).toBe(true);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(1);
-        expect(res.body.links[0].link).toEqual(linkDto.link);
-        expect(res.body.links[0].hasOwnProperty('date')).toBe(true)
-        expect(res.body.links[0].hasOwnProperty('uuid')).toBe(true)
-    });
+  it('returns 200 with multipe links when exist two', async () => {
+    await LinkFactory.create();
+    await LinkFactory.create();
 
-    it("returns 200 with multipe links when exist two", async () => {
-        await LinkFactory.create();
-        await LinkFactory.create();
+    const res = await request(app).get('/links');
 
-        const res = await request(app).get("/links");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(2);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(2);
-    });
+  it('by default return 20 link randomly', async () => {
+    const totalLinks = 25;
+    await LinkFactory.createX(totalLinks);
 
-    it("by default return 20 link randomly", async () => {
-        const totalLinks = 25;
-        await LinkFactory.createX(totalLinks);
+    const res = await request(app).get('/links');
 
-        const res = await request(app).get("/links");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(totalLinks);
+    expect(res.body.links.length).toEqual(20);
+    const maxId = Math.max(
+      ...res.body.links.map((links: { id: any }) => links.id),
+    );
+    expect(res.body.links[0].id).toBeLessThanOrEqual(maxId);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(totalLinks);
-        expect(res.body.links.length).toEqual(20);
-        const maxId = Math.max(...res.body.links.map((links: { id: any; }) => links.id));
-        expect(res.body.links[0].id).toBeLessThanOrEqual(maxId);
-    });
+  it('returns 200 sorted by newest first', async () => {
+    await LinkFactory.create();
+    await LinkFactory.create();
 
-    it("returns 200 sorted by newest first", async () => {
-        await LinkFactory.create();
-        await LinkFactory.create();
+    const res = await request(app).get('/links?sort=-date');
 
-        const res = await request(app).get("/links?sort=-date");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.links[0].id).toBeGreaterThan(res.body.links[1].id);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.links[0].id).toBeGreaterThan(res.body.links[1].id);
-    });
+  it('returns 200 returns empty filtering for something does not exist', async () => {
+    const category = await CategoryFactory.create('Environment');
+    await LinkFactory.create([category]);
 
-    it("returns 200 returns empty filtering for something does not exist", async () => {
-        const category = await CategoryFactory.create('Environment');
-        await LinkFactory.create([category]);
+    const res = await request(app).get('/links?categories=technology');
 
-        const res = await request(app).get("/links?categories=technology");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(0);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(0);
-    });
+  it('returns 200 filtering for something does exist', async () => {
+    const slug = 'environment';
+    const category = await CategoryFactory.create('Environment', slug);
+    await LinkFactory.create([category]);
 
-    it("returns 200 filtering for something does exist", async () => {
-        const slug = 'environment';
-        const category = await CategoryFactory.create('Environment', slug);
-        await LinkFactory.create([category]);
+    const res = await request(app).get('/links?categories=' + slug);
 
-        const res = await request(app).get("/links?categories=" + slug);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(1);
+    expect(res.body.links[0].categories[0].name).toEqual(category.name);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(1);
-        expect(res.body.links[0].categories[0].name).toEqual(category.name);
-    });
+  it('returns 200 sorting by date and filtering by multiple categories', async () => {
+    const category1 = await CategoryFactory.create('Environment', 'env1');
+    await LinkFactory.create([category1]);
 
-    it("returns 200 sorting by date and filtering by multiple categories", async () => {
-        const category1 = await CategoryFactory.create('Environment', 'env1');
-        await LinkFactory.create([category1]);
+    const category2 = await CategoryFactory.create('Environment2', 'env2');
+    await LinkFactory.create([category2]);
 
-        const category2 = await CategoryFactory.create('Environment2', 'env2');
-        await LinkFactory.create([category2]);
+    const category3 = await CategoryFactory.create('Environment3', 'env3');
+    await LinkFactory.create([category3]);
 
-        const category3 = await CategoryFactory.create('Environment3', 'env3');
-        await LinkFactory.create([category3]);
+    const filter = category1.slug + ',' + category3.slug;
+    const res = await request(app).get(
+      '/links?categories=' + filter + '&sort=-date',
+    );
 
-        const filter = category1.slug + ',' + category3.slug;
-        const res = await request(app).get("/links?categories=" + filter + "&sort=-date");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(2);
+    expect(res.body.links[0].categories[0].name).toEqual(category3.name);
+    expect(res.body.links[1].categories[0].name).toEqual(category1.name);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(2);
-        expect(res.body.links[0].categories[0].name).toEqual(category3.name);
-        expect(res.body.links[1].categories[0].name).toEqual(category1.name);
-    });
+  it('returns 200 sorting randomly and filtering by multiple categories', async () => {
+    const category1 = await CategoryFactory.create('Environment', 'env1');
+    const category2 = await CategoryFactory.create('Environment2', 'env2');
+    const category3 = await CategoryFactory.create('Environment3', 'env3');
 
-    it("returns 200 sorting randomly and filtering by multiple categories", async () => {
-        const category1 = await CategoryFactory.create('Environment', 'env1');
-        const category2 = await CategoryFactory.create('Environment2', 'env2');
-        const category3 = await CategoryFactory.create('Environment3', 'env3');
+    await LinkFactory.createX(5, [category1]);
+    await LinkFactory.createX(5, [category2]);
+    await LinkFactory.createX(5, [category3]);
 
-        await LinkFactory.createX(5, [category1]);
-        await LinkFactory.createX(5, [category2]);
-        await LinkFactory.createX(5, [category3]);
+    const filter = category1.slug + ',' + category3.slug;
+    const res = await request(app).get('/links?categories=' + filter);
 
-        const filter = category1.slug + ',' + category3.slug;
-        const res = await request(app).get("/links?categories=" + filter);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(10);
+    const maxId = Math.max(
+      ...res.body.links.map((links: { id: any }) => links.id),
+    );
+    expect(res.body.links[0].id).toBeLessThanOrEqual(maxId);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(10);
-        const maxId = Math.max(...res.body.links.map((links: { id: any; }) => links.id));
-        expect(res.body.links[0].id).toBeLessThanOrEqual(maxId);
-    });
+  it('returns 200 filtering by category with multiple words', async () => {
+    const slug = 'business-and-economy';
+    const category1 = await CategoryFactory.create('Environment', 'env');
+    const category2 = await CategoryFactory.create('Business & Economy', slug);
 
-    it("returns 200 filtering by category with multiple words", async () => {
-        const slug = 'business-and-economy';
-        const category1 = await CategoryFactory.create('Environment', 'env');
-        const category2 = await CategoryFactory.create('Business & Economy', slug);
+    await LinkFactory.createX(5, [category1]);
+    await LinkFactory.createX(5, [category2]);
 
-        await LinkFactory.createX(5, [category1]);
-        await LinkFactory.createX(5, [category2]);
+    const res = await request(app).get('/links?categories=' + category2.slug);
 
-        const res = await request(app).get("/links?categories=" + category2.slug);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(5);
+    expect(res.body.links[0].categories[0].slug).toEqual(slug);
+  });
 
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(5);
-        expect(res.body.links[0].categories[0].slug).toEqual(slug);
-    });
+  it('returns 6 links from second page sorting by date', async () => {
+    await LinkFactory.createX(12);
 
-    it("returns 6 links from second page sorting by date", async () => {
-        await LinkFactory.createX(12);
+    const res = await request(app).get('/links?sort=-date&page=2');
 
-        const res = await request(app).get("/links?sort=-date&page=2");
-
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.total).toEqual(12);
-        expect(res.body.links.length).toEqual(6);
-        expect(res.body.links[0].title).toEqual('title5');
-        expect(res.body.links[1].title).toEqual('title4');
-    });
-
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.total).toEqual(12);
+    expect(res.body.links.length).toEqual(6);
+    expect(res.body.links[0].title).toEqual('title5');
+    expect(res.body.links[1].title).toEqual('title4');
+  });
 });
