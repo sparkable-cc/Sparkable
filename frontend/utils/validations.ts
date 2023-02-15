71065e817c126867ef2b69d3a20133ee513577e3
tutorial <aumarh@tutorial.net>
2023-02-15 15:14:46 +0100

    Fix password pattern and eroor message bug

--- frontend/utils/validations.ts
+++ frontend/utils/validations.ts
@@ -5,27 +5,25 @@ export const signUpSchema = Joi.object({
     .email({ tlds: { allow: false } })
     .required(),
 
-  username: Joi.string()
-    .alphanum()
-    .min(3)
-    .max(30)
-    .required(),
+  username: Joi.string().alphanum().min(3).max(30).required(),
 
   password: Joi.string()
     .min(8)
-    .pattern(new RegExp(/^[a-zA-Z0-9]{3,30}$/))
-    .required(),
+    .pattern(new RegExp('^[a-zA-Z0-9!@#$%&*,]{3,30}$'))
+    .required()
+    .messages({
+      'string.pattern.base':
+        'Password must contain at least one number and one letter',
+    }),
 });
 
 export const signInSchema = Joi.object({
-  login: Joi.string()
-    .required(),
+  login: Joi.string().required(),
 
-  password: Joi.string()
-    .required(),
+  password: Joi.string().required(),
 });
 
 export const validationInitialState = {
-  field: "",
-  message: ""
-}
+  field: '',
+  message: '',
+};
