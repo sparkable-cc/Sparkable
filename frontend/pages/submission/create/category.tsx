81c82cad035e823e26c9facbdba33a6bc625fcf3
Dima <dimakrsna@gmail.com>
2023-02-21 12:56:33 +0200

    Submission - category tab

--- frontend/pages/submission/create/category.tsx
+++ frontend/pages/submission/create/category.tsx
@@ -1,17 +1,30 @@
-import { useState, FormEvent, useCallback } from "react";
+import { FormEvent, useMemo, useEffect } from "react";
 import { CreateSubmissionLayout } from "../../../layouts/CreateSubmissionLayout";
 import styles from '../../../styles/Submission.module.scss';
 import { useRouter } from "next/router";
 import { FormInput } from "../../../components/FormInput";
 import { useAppSelector, useAppDispatch } from "../../../store/hooks";
-import { setLink, selectLink } from "../../../store/submissionSlice";
+import { getCategories, useLazyGetCategoriesQuery } from "../../../store/api";
+import classNames from "classnames";
+import { v4 as uuidv4 } from "uuid";
+import { 
+  setCategories, 
+  selectCategories, 
+  setSuggestedCategory, 
+  selectSuggestedCategory,
+} from "../../../store/submissionSlice";
 
 
 const CreateSubmissionCategory = () => {
-  // const link = useAppSelector(selectLink)
-  // const [value, setValue] = useState(link);
-  // const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
   const router = useRouter();
+  const [triggerGetCategories] = useLazyGetCategoriesQuery();
+
+  const categoriesMemo = useMemo(() => getCategories.select(), []);
+  const categories = useAppSelector(categoriesMemo);
+  const categoriesData = categories?.data?.categories;
+  const activeCategories = useAppSelector(selectCategories);
+  const suggestedCategory = useAppSelector(selectSuggestedCategory);
 
   const onButtonClick = () => {
     router.push("/submission/create/statement")
@@ -19,19 +32,36 @@ const CreateSubmissionCategory = () => {
 
   const onInputChange = (event: FormEvent<HTMLInputElement>) => {
     const { value } = event.currentTarget;
-    // setValue(value);
+    console.log('value', value)
+    dispatch(setSuggestedCategory(value));
   };
 
   const onInputClear = () => {
-    // dispatch(setLink(value))
-    // setValue("");
+    dispatch(setSuggestedCategory(""));
   }
+
+  const onSetCurrentCategory = (event: any) => {
+    const param = event?.target?.getAttribute("data-param");
+    if (!param) return;
+
+    if (activeCategories?.find(item => item === param)) {
+      dispatch(setCategories(activeCategories.filter(item => item !== param)));
+    } else {
+      dispatch(setCategories([...activeCategories, ...[param]]));
+    }
+  };
+
+  useEffect(() => {
+    if (!categoriesData) {
+      triggerGetCategories();
+    }
+  });
 
   return (
     <CreateSubmissionLayout
       submitButtonText="Continue"
       onSubmit={onButtonClick}
-      isSubmitAvailable={true}
+      isSubmitAvailable={Boolean(activeCategories?.length)}
       isCancelAvailable={true}
     >
       <div className={styles.categoryDescription}>
@@ -41,8 +71,24 @@ const CreateSubmissionCategory = () => {
       <header className={styles.categoryHeader}>
         Categories
       </header>
+      <div className={styles.categorysWrapper}>
+        {
+          categoriesData && categoriesData?.map(item => (
+            <button
+              className={classNames(styles.categoryButton, {
+                [styles.active]: activeCategories.find(sortItem => sortItem === item.slug)
+              })}
+              onClick={onSetCurrentCategory}
+              key={uuidv4()}
+              data-param={item.slug}
+            >
+              {item.name}
+            </button>
+          ))
+        }
+      </div>
       <FormInput
-        value={""}
+        value={suggestedCategory}
         id="suggestion"
         name="suggestion"
         label=""
@@ -51,6 +97,9 @@ const CreateSubmissionCategory = () => {
         onClear={onInputClear}
         errorMessage={""}
       />
+      <div className={styles.inputNote}>
+        Your suggestion helps Sparkable introduce new categories and will not be visible to others.
+      </div>
     </CreateSubmissionLayout>
   )
 }
--- frontend/store/submissionSlice.ts
+++ frontend/store/submissionSlice.ts
@@ -3,10 +3,14 @@ import type { RootState } from "./index";
 
 export interface SubmissionState {
   link: string;
+  categories: string[];
+  suggestedCategory: string
 }
 
 const initialState: SubmissionState = {
   link: "",
+  categories: [],
+  suggestedCategory: "",
 };
 
 export const submissionState = createSlice({
@@ -16,13 +20,23 @@ export const submissionState = createSlice({
     setLink: (state, action: PayloadAction<string>) => {
       state.link = action.payload;
     },
+    setCategories: (state, action: PayloadAction<string[]>) => {
+      state.categories = action.payload;
+    },
+    setSuggestedCategory: (state, action: PayloadAction<string>) => {
+      state.suggestedCategory = action.payload;
+    },
   },
 });
 
 export const {
   setLink,
+  setCategories,
+  setSuggestedCategory,
 } = submissionState.actions;
 
 export const selectLink = (state: RootState) => state.submission.link;
+export const selectCategories = (state: RootState) => state.submission.categories;
+export const selectSuggestedCategory = (state: RootState) => state.submission.suggestedCategory;
 
 export default submissionState.reducer;
