cc4144ea18af2f7006fded596db165c69d231d09
Dima <dimakrsna@gmail.com>
2023-02-28 17:02:36 +0200

    Submission unlogged message

--- frontend/pages/submission/create/link.tsx
+++ frontend/pages/submission/create/link.tsx
@@ -4,7 +4,7 @@ import styles from "../../../styles/Submission.module.scss";
 import { useRouter } from "next/router";
 import { FormInput } from "../../../components/FormInput";
 import { useAppSelector, useAppDispatch } from "../../../store/hooks";
-import { setLink, selectLink, setLinkData } from "../../../store/submissionSlice";
+import { setLink, selectLink, setLinkData, selectLinkData } from "../../../store/submissionSlice";
 import { useLazyPostLinkPreviewQuery } from "../../../store/api/submissionApi";
 import debounce from "lodash/debounce";
 import { LinkPreview } from "../../../components/LinkPreview";
@@ -12,13 +12,17 @@ import { ModalNote } from "../../../components/ModalNote";
 import Link from "next/link";
 import { storageKeys } from "../../../utils/storageKeys";
 import { toast } from "react-toastify";
+import { checkCredentials } from "../../../utils/checkCredentials";
+import { UnloggedMessage } from "../../../components/UnloggedMessage";
 
 const CreateSubmissionLink = () => {
   const link = useAppSelector(selectLink);
+  const linkData = useAppSelector(selectLinkData);
   const [ value, setValue ] = useState(link);
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const [ triggerPostLinkPreview, { isFetching, data }] = useLazyPostLinkPreviewQuery();
+  const [ isLogged, setLogged ] = useState(false);
+  const [ triggerPostLinkPreview, { isFetching }] = useLazyPostLinkPreviewQuery();
 
   const onButtonClick = () => {
     router.push("/submission/create/category");
@@ -28,15 +32,19 @@ const CreateSubmissionLink = () => {
     dispatch(setLink(value));
     sessionStorage.setItem(storageKeys.submissionLink, value);
 
-    try {
-      triggerPostLinkPreview(value).then((res: any) => {
-        if (res?.error) {
-          toast.error(res?.error?.data?.message);
-        }
-      });
-    } catch (error: any) {
-      toast.error(error?.message);
-    }
+    const request = triggerPostLinkPreview(value);
+
+    request.then((res: any) => {
+      if(res.data){
+        dispatch(setLinkData(res.data));
+        sessionStorage.setItem(storageKeys.submissionLinkData, JSON.stringify(res.data));
+      }
+      if (res?.error) {
+        toast.error(res?.error?.data?.message);
+      }
+    });
+
+    setTimeout(() => request?.abort(), 3000);
   };
 
   const debouncedHandler = useCallback(debounce(debounceSetLink, 1000), []);
@@ -48,12 +56,13 @@ const CreateSubmissionLink = () => {
   };
 
   const onInputClear = () => {
+    dispatch(setLinkData(null));
     dispatch(setLink(value));
     setValue("");
   };
 
   const isPreviewAvailable = (): boolean => {
-    if (value && data) return true;
+    if (value && linkData) return true;
 
     else {
       return false;
@@ -67,11 +76,11 @@ const CreateSubmissionLink = () => {
   }, []);
 
   useEffect(() => {
-    if(data){
-      setLinkData(data);
-      sessionStorage.setItem(storageKeys.submissionLinkData, JSON.stringify(data));
+    if (checkCredentials()) {
+      setLogged(true);
     }
-  }, [data]);
+  }, [linkData]);
+
 
   return (
     <CreateSubmissionLayout
@@ -80,37 +89,44 @@ const CreateSubmissionLink = () => {
       isSubmitAvailable={isPreviewAvailable()}
       isCancelAvailable
     >
-      <FormInput
-        value={value}
-        id="link"
-        name="link"
-        label="Paste your link here:"
-        placeholder="https://www.site.com"
-        onChange={onInputChange}
-        onClear={onInputClear}
-        errorMessage=""
-      />
-      <div>
-        <header className={styles.linkPreviewHeader}>
-          <span>Link preview</span>
-          <ModalNote title="Link preview">
-            <div className={styles.modalText}>We generate the link preview based on each link’s public metadata.</div>
-            <div className={styles.modalText}>Something is wrong or not working as expected?</div>
-            <div className={styles.modalText}>
-              <Link href="/" className={styles.modalLink}>Contact us</Link>
+      {
+        isLogged ?
+          <>
+            <FormInput
+              value={value}
+              id="link"
+              name="link"
+              label="Paste your link here:"
+              placeholder="https://www.site.com"
+              onChange={onInputChange}
+              onClear={onInputClear}
+              errorMessage=""
+            />
+            <div>
+              <header className={styles.linkPreviewHeader}>
+                <span>Link preview</span>
+                <ModalNote title="Link preview">
+                  <div className={styles.modalText}>We generate the link preview based on each link’s public metadata.</div>
+                  <div className={styles.modalText}>Something is wrong or not working as expected?</div>
+                  <div className={styles.modalText}>
+                    <Link href="/" className={styles.modalLink}>Contact us</Link>
+                  </div>
+                </ModalNote>
+              </header>
+              <div className={styles.linkPreviewWrapper}>
+                <LinkPreview
+                  isLoading={isFetching}
+                  site={linkData?.ogUrl}
+                  title={linkData?.ogTitle}
+                  description={linkData?.ogDescription}
+                  image={linkData?.ogImage[0]?.url}
+                />
+              </div>
             </div>
-          </ModalNote>
-        </header>
-        <div className={styles.linkPreviewWrapper}>
-          <LinkPreview
-            isLoading={isFetching}
-            site={data?.ogUrl}
-            title={data?.ogTitle}
-            description={data?.ogDescription}
-            image={data?.ogImage[0]?.url}
-          />
-        </div>
-      </div>
+          </> :
+          <UnloggedMessage />
+      }
+
     </CreateSubmissionLayout>
   );
 };
