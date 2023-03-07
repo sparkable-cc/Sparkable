c9a174757d5b4d90e51b469a0befd4e5bb182fc3
Dima <dimakrsna@gmail.com>
2023-03-07 13:14:35 +0200

    Link tracker improvements

--- frontend/components/ArticleLink/index.tsx
+++ frontend/components/ArticleLink/index.tsx
@@ -2,7 +2,7 @@ import styles from "./index.module.scss";
 import React from "react";
 import { useLazyPostViewedLinkByUserDataQuery } from "../../store/api/trackingApi";
 import { toast } from "react-toastify";
-import { storageKeys } from "../../../utils/storageKeys";
+import { storageKeys } from "../../utils/storageKeys";
 
 interface Props {
   link: string;
@@ -10,20 +10,25 @@ interface Props {
 }
 
 export const ArticleLink = ({ link, uuid }: Props) => {
+  const [triggerPostViewedLinkByUserData, { isLoading, data }] = useLazyPostViewedLinkByUserDataQuery();
 
-  const userUuid = sessionStorage.getItem(storageKeys.userId);
-  if (userUuid) {
-    const [ triggerPostViewedLinkByUserData, { isLoading, data }] = useLazyPostViewedLinkByUserDataQuery();
+  const onClick = () => {
+    const userUuid = sessionStorage.getItem(storageKeys.userId);
+    if (!userUuid) return;
+
     try {
       const data = {
         userUuid,
         linkUuid: uuid
       };
+
       triggerPostViewedLinkByUserData(data).then((res: any) => {
         if (res?.error) {
+          console.log('res', res)
           toast.error(res?.error?.data?.message);
         }
       });
+
     } catch (error: any) {
       toast.error(error?.message);
     }
@@ -34,6 +39,7 @@ export const ArticleLink = ({ link, uuid }: Props) => {
       target="_blank"
       rel="noopener noreferrer"
       href={link}
+      onClick={onClick}
       className={styles.articleLink}
     >
       {link}
