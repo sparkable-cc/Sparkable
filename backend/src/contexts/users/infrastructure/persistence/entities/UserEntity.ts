7084feafbe75cc06a2484c76c3e1ece717e7ae79
Hynnot <hynnot@gmail.com>
2023-03-16 14:41:33 +0100

    Save registration date when an user is created

--- backend/src/contexts/users/infrastructure/persistence/entities/UserEntity.ts
+++ backend/src/contexts/users/infrastructure/persistence/entities/UserEntity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm"
 import { UserDto } from "../../../domain/models/UserDto";
 
 @Entity('users')
@@ -17,4 +17,7 @@ export class UserEntity implements UserDto {
 
   @Column({nullable: true})
   uuid: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  registrationDate: Date;
 }
