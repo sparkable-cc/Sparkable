5c1a9aad7641aad7d7eac44d3ce35b0f6f36fa0a
tutorial <aumarh@tutorial.net>
2023-04-05 09:44:54 +0200

    Add metatags for SEO

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,31 @@
-import "../styles/globals.scss";
-import "react-toastify/dist/ReactToastify.css";
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { MainLayout } from "../layouts/MainLayout";
-import { store } from "../store/index";
-import { ToastContainer } from "react-toastify";
-import { NoAccess } from "../components/NoAccess";
-import { checkCredentials } from "../utils/checkCredentials";
-import Head from "next/head";
+import '../styles/globals.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import { NoAccess } from '../components/NoAccess';
+import { MainLayout } from '../layouts/MainLayout';
+import { store } from '../store/index';
+import { checkCredentials } from '../utils/checkCredentials';
 
 export default function App({ Component, pageProps }: AppProps) {
   <Head>
     <link rel="shortcut icon" href="/public/favicon.ico" />
+    <title>Sparkable</title>
+    <meta
+      name="description"
+      content="Discover links that spark new understanding."
+    />
+    <meta property="og:title" content="Sparkable" />
+    <meta
+      property="og:description"
+      content="Discover links that spark new understanding."
+    />
+    <meta
+      property="og:image"
+      content="https://sparkable.cc/public/og-image.png"
+    />
   </Head>;
 
   if (pageProps.protected && !checkCredentials()) {
