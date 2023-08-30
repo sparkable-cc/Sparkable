a45f14e852879ef5d3e33a60cba98fdf338fe6ca
Hynnot <hynnot@gmail.com>
2023-08-30 13:43:14 +0200

    Add info message in modal about https

--- frontend/pages/submission/create/link.tsx
+++ frontend/pages/submission/create/link.tsx
@@ -105,9 +105,9 @@ const CreateSubmissionLink = () => {
             <div>
               <header className={styles.linkPreviewHeader}>
                 <span>Link preview</span>
-                <span>INFORM IT's MANDATORY HTTPS</span>
                 <ModalNote title="Link preview">
                   <div className={styles.modalText}>We generate the link preview based on each link’s public metadata.</div>
+                  <div className={styles.modalText}>Only works with secure links (https).</div>
                   <div className={styles.modalText}>Something is wrong or not working as expected?</div>
                   <div className={styles.modalText}>
                     <Link href="/" className={styles.modalLink}>Contact us</Link>
